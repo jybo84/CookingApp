13cd7262c241eda3946a161088e34acf476c60e1
Maxim <jybo@mail.ru>
2024-03-13 16:40:40 +1000

    RA-04 работа с пустым стартовым фрагментом

--- app/src/main/java/com/example/androidstudyapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidstudyapp/CategoriesListFragment.kt
@@ -5,21 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.androidstudyapp.databinding.ActivityMainBinding
 import com.example.androidstudyapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment() {
 
-    private var _binding: FragmentListCategoriesBinding? = null
-    private val binding get() = _binding
+    private var binding: FragmentListCategoriesBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding?.root
 
     }
-
 }
