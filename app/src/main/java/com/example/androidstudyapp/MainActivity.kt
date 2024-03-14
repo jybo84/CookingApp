733e7bdc5e2c4491534ffec3c91fda06b5db8621
Maxim <jybo@mail.ru>
2024-03-14 16:39:18 +1000

    RA-03 работа с binding, корректировки id, shape, dimens

--- app/src/main/java/com/example/androidstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/MainActivity.kt
@@ -8,15 +8,13 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.androidstudyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private var _binding: ActivityMainBinding? = null
-    private val binding get() = _binding
 
-    //    private val binding: ActivityMainBinding by lazy(ActivityMainBinding::inflate()) //TODO надо как-то докрутить
+     private val binding by lazy{ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        _binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding?.root)
+
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
