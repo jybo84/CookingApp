d55a277492283df9ef63ecb931291ad5c8efd5e7
Maxim <jybo@mail.ru>
2024-03-16 01:06:15 +1000

    RA-03 работа с Fragment

--- app/src/main/java/com/example/androidstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.example.androidstudyapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.commit
-//import androidx.core.graphics.Insets.add
-//import androidx.core.view.OneShotPreDrawListener.add
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import com.example.androidstudyapp.databinding.ActivityMainBinding
-import com.example.androidstudyapp.databinding.FragmentListCategoriesBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         if (savedInstanceState == null) {
-            supportFragmentManager.commit{
+            supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<CategoriesListFragment>(R.id.mainContainer)
             }
@@ -30,23 +20,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 }
 
-//class MainActivity : AppCompatActivity() {
-//    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
-//        setContentView(binding.root)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-//
-//        val fragmentTransaction = supportFragmentManager.beginTransaction()
-//        fragmentTransaction.add(R.id.mainContainer, CategoriesListFragment()).commit()
-//    }
-//}
+
 
 
 
