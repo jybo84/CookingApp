d40669b9bdec811e1d1fd0e128760e11940d45de
Maxim <jybo@mail.ru>
2024-03-16 00:56:58 +1000

    RA-03 работа с Fragment

--- app/src/main/java/com/example/androidstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/MainActivity.kt
@@ -3,10 +3,32 @@ package com.example.androidstudyapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.OneShotPreDrawListener.add
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.commit
+//import androidx.core.graphics.Insets.add
+//import androidx.core.view.OneShotPreDrawListener.add
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
 import com.example.androidstudyapp.databinding.ActivityMainBinding
+import com.example.androidstudyapp.databinding.FragmentListCategoriesBinding
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit{
+                setReorderingAllowed(true)
+                add<CategoriesListFragment>(R.id.mainContainer)
+            }
+        }
+    }
+}
 
 //class MainActivity : AppCompatActivity() {
 //    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -26,16 +48,5 @@ import com.example.androidstudyapp.databinding.ActivityMainBinding
 //    }
 //}
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<CategoriesListFragment>(R.id.mainContainer)
-            }
-        }
-    }
-}
+
 
