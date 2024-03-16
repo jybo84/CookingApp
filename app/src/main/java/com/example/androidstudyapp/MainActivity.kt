1a87f146f547bf54450fb498a10f78cf04fd90eb
Maxim <jybo@mail.ru>
2024-03-16 23:13:24 +1000

    RA-03 работа с Fragment

--- app/src/main/java/com/example/androidstudyapp/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.androidstudyapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.commit
 import androidx.fragment.app.add
 import com.example.androidstudyapp.databinding.ActivityMainBinding
@@ -11,6 +13,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
