f91005d5fa9cbb0876e9644bd0d6d3cdf48d8f67
Maxim <jybo@mail.ru>
2024-03-28 15:53:27 +1000

    RA-08 RecyclerView со списком категорий

--- app/src/main/java/com/example/androidstudyapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/androidstudyapp/CategoriesListAdapter.kt
@@ -3,31 +3,34 @@ package com.example.androidstudyapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidstudyapp.databinding.ItemCategoryBinding
 
-class CategoriesListAdapter(private val dataSet: Category) :
-    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.item_category, parent, false)
-        return ViewHolder(view)
+class CategoriesListAdapter(private val dataSet: List<Category>): RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val binding = ItemCategoryBinding.inflate(LayoutInflater.from(itemView.context))
+        val ivCategory1 = binding.ivCategory
+        val tvTitleCategory1 = binding.tvTitleCategory
+        val tvDescriptionCategory1 = binding.tvDescriptionCategory
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val zzz = dataSet[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val el = LayoutInflater.from(parent.context).inflate(R.layout.item_category, parent, false)
+        return ViewHolder(el)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, numberOfPosition: Int) {
+        val category: Category = dataSet[numberOfPosition]
+        holder.tvTitleCategory1.text = category.title
+        holder.tvDescriptionCategory1.text = category.description
     }
 
     override fun getItemCount() = dataSet.size
-}
+    }
 
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    private val binding = ItemCategoryBinding.inflate(LayoutInflater.from(itemView.context))
-    val ivCategory = binding.ivCategory
-    val titleCategory = binding.titleCategory
-    val descriptionCategory = binding.descriptionCategory
-}
+
+
 
 
--- app/src/main/java/com/example/androidstudyapp/Category.kt
+++ app/src/main/java/com/example/androidstudyapp/Category.kt
@@ -2,6 +2,6 @@ package com.example.androidstudyapp
 data class Category(
     val id: Int,
     val title: String,
-    val description: String,
     val imageUrl: String,
+    val description: String,
 )
