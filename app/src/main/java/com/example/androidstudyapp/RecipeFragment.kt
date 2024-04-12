e755a29314eb44dbe232e02bede991114648d137
Maxim <jybo@mail.ru>
2024-04-12 11:17:23 +1000

    RA-11 экран со списком рецептов

--- app/src/main/java/com/example/androidstudyapp/RecipeFragment.kt
+++ app/src/main/java/com/example/androidstudyapp/RecipeFragment.kt
@@ -9,13 +9,24 @@ import com.example.androidstudyapp.databinding.FragmentRecipeBinding
 
 class RecipeFragment : Fragment() {
 
-    val binding by lazy { FragmentRecipeBinding.inflate(layoutInflater) }
+    private val binding by lazy { FragmentRecipeBinding.inflate(layoutInflater) }
+    private var categoryRecipeId: Int? = null
+    private var categoryName: String? = null
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipe, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        categoryRecipeId = arguments?.getInt("ARG_CATEGORY_ID")
+        categoryName = arguments?.getString("ARG_CATEGORY_NAME")
+
     }
 }
