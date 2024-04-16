8d9103d2ce79b0621cf907af01568d2c4052fd9c
Maxim <jybo@mail.ru>
2024-04-16 21:05:31 +1000

    RA-12 работа с Parcelable

--- app/src/main/java/com/example/androidstudyapp/RecipeFragment.kt
+++ app/src/main/java/com/example/androidstudyapp/RecipeFragment.kt
@@ -1,11 +1,9 @@
 package com.example.androidstudyapp
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.androidstudyapp.databinding.FragmentRecipeBinding
 
@@ -23,7 +21,6 @@ class RecipeFragment : Fragment() {
         return binding.root
     }
 
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
