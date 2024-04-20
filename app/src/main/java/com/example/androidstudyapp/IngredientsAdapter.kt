d63a0e680427ec53623f51509a7f94694519d2f0
Maxim <jybo@mail.ru>
2024-04-20 23:14:40 +1000

    RA-13 реализация экрана рецепта без механизма подсчета

--- app/src/main/java/com/example/androidstudyapp/IngredientsAdapter.kt
+++ app/src/main/java/com/example/androidstudyapp/IngredientsAdapter.kt
@@ -8,7 +8,6 @@ import com.example.androidstudyapp.databinding.ItemIngredientBinding
 class IngredientsAdapter(private val dataSet: List<Ingredient>) :
     RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
 
-
     class ViewHolder(binding: ItemIngredientBinding) : RecyclerView.ViewHolder(binding.root) {
         val nameOfIngredient = binding.nameOfIngredient
         val quantityAndUnitOfMeasure = binding.quantityAndUnitOfMeasure
@@ -31,30 +30,7 @@ class IngredientsAdapter(private val dataSet: List<Ingredient>) :
         holder.nameOfIngredient.text = ingredient.description
         holder.quantityAndUnitOfMeasure.text =
             holder.itemView.resources.getString(
-                R.string.ingr_accaunt, ingredient.quantity, ingredient.unitOfMeasure
+                R.string.ingredient_account, ingredient.quantity, ingredient.unitOfMeasure
             )
     }
 }
-
-
-//class IngredientsAdapter(private val dataSet: List<Ingredient>): RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
-//
-//    class ViewHolder(private val binding: ItemIngredientBinding): RecyclerView.ViewHolder(binding.root){
-//        fun bind(ing: Ingredient){
-//            binding.nameOfIngredient.text = ing.description
-//            binding.quantityAndUnitOfMeasure.text = itemView.resources.getString(R.string.ingr_accaunt, ing.quantity, ing.unitOfMeasure )
-//        }
-//    }
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-//        return ViewHolder(ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent,false))
-//    }
-//
-//    override fun getItemCount(): Int {
-//        return dataSet.size
-//    }
-//
-//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-//    holder.bind(dataSet[position])
-//    }
-//}
