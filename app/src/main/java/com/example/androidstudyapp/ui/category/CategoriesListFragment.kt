350230274c75fb30c696ea05b35890c11b21469d
Maxim <jybo@mail.ru>
2024-05-31 08:49:20 +1000

    RA-28 доработки по проверке

--- app/src/main/java/com/example/androidstudyapp/ui/category/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidstudyapp/ui/category/CategoriesListFragment.kt
@@ -52,15 +52,6 @@ class CategoriesListFragment : Fragment() {
         val bundle = bundleOf(
             ARG_CATEGORY_ID to categoryId,
         )
-//        parentFragmentManager.commit {
-//            replace<RecipesListFragment>(R.id.mainContainer, args = bundle)
-//            setReorderingAllowed(true)
-//            addToBackStack(null)
-
-//        }
-        findNavController().navigate(R.id.action_categoriesListFragment_to_recipesListFragment, bundle)
+        findNavController().navigate(R.id.action_categoriesListFragment_to_recipesListFragment, bundle )
     }
-
-
 }
-
