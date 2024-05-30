186e0f64296544907f99dc899744e7ef8a6a1c2d
Maxim <jybo@mail.ru>
2024-05-30 16:13:37 +1000

    RA-28 работа с Navigation

--- app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import com.example.androidstudyapp.R
 import com.example.androidstudyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    private var navCont: NavController =
+        (supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment).navController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -20,16 +23,16 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val navController = findNavController(R.id.mainContainer)
+        navCont = findNavController(R.id.mainContainer)
 
         binding.apply {
             buttonFavourites.setOnClickListener {
-                navController.navigate(
+                navCont.navigate(
                     R.id.favouritesFragment
                 )
             }
             buttonCategory.setOnClickListener {
-                navController.navigate(
+                navCont.navigate(
                     R.id.categoriesListFragment
                 )
             }
