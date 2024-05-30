73244bb02b06336b3484453fcd58990628207409
Maxim <jybo@mail.ru>
2024-05-30 17:05:10 +1000

    RA-28 работа с Navigation

--- app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
@@ -8,31 +8,29 @@ import androidx.navigation.findNavController
 import com.example.androidstudyapp.R
 import com.example.androidstudyapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-//    private var navController: NavController =
-//        (supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment).navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-    binding.apply {
-        buttonFavourites.setOnClickListener {
-            val navController = findNavController(R.id.mainContainer)
-            navController.navigate(R.id.favouritesFragment)
-        }
-        buttonCategory.setOnClickListener {
-            val navController = findNavController(R.id.mainContainer)
-            navController.navigate(R.id.categoriesListFragment)
-        }
+        binding.apply {
+            buttonFavourites.setOnClickListener {
+                val navController = findNavController(R.id.mainContainer)
+                navController.navigate(R.id.favouritesFragment)
+            }
+            buttonCategory.setOnClickListener {
+                val navController = findNavController(R.id.mainContainer)
+                navController.navigate(R.id.categoriesListFragment)
+            }
         }
     }
 }
-
-
