7b1b81c256c0367de018257b1e8c5286f62e5ba4
Maxim <jybo@mail.ru>
2024-05-31 16:20:07 +1000

    RA-29 работа по коду

--- app/src/main/java/com/example/androidstudyapp/ui/recipe/favourites/FavouritesFragment.kt
+++ app/src/main/java/com/example/androidstudyapp/ui/recipe/favourites/FavouritesFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.example.androidstudyapp.R
-import com.example.androidstudyapp.data.ARG_RECIPE_ID
 import com.example.androidstudyapp.data.Recipe
 import com.example.androidstudyapp.databinding.FragmentFavouritesBinding
 import com.example.androidstudyapp.ui.recipe.RecipesListAdapter
@@ -53,7 +50,8 @@ class FavouritesFragment : Fragment() {
     }
 
     fun openRecipeByRecipeId(id: Int) {
-        val bundle = bundleOf(ARG_RECIPE_ID to id)
-        findNavController().navigate(R.id.recipeFragment, bundle)
+        val actions =
+            FavouritesFragmentDirections.actionFavouritesFragmentToRecipeFragment(recipeId = id)
+        findNavController().navigate(actions)
     }
 }
