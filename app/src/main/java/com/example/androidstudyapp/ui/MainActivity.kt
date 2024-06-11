3dafc1c46efc5ca9a28c861b6d9070df524b159b
Maxim <jybo@mail.ru>
2024-06-11 15:42:58 +1000

    RA-31 URLConnection до работы с массивом

--- app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/androidstudyapp/ui/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.androidstudyapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import com.example.androidstudyapp.R
+import com.example.androidstudyapp.data.Category
 import com.example.androidstudyapp.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,5 +34,43 @@ class MainActivity : AppCompatActivity() {
 
             buttonCategory.setOnClickListener { navController.navigate(R.id.categoriesListFragment) }
         }
+
+        Log.i("MyLog", "Метод onCreate() выполняется на потоке: Main")
+
+
+        val myThread = Thread {
+            val url = URL("https://recipes.androidsprint.ru/api/category")
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            val body = "responseBody: ${connection.inputStream.bufferedReader().readText()}"
+            Log.i("MyLog", body)
+            Log.i("MyLog", "Выполняю запрос в потоке myThread")
+            Log.i("MyLog", "_________________________________")
+
+
+
+
+
+        }
+
+        myThread.start()
     }
+
+    private fun parseResponse(response: String) {
+        val responseObject = JSONObject(response)
+        val item = Category(
+            responseObject.getJSONObject("responseBody").getInt("id"),
+            responseObject.getJSONObject("responseBody").getString("title"),
+            responseObject.getJSONObject("responseBody").getString("description"),
+            responseObject.getJSONObject("responseBody").getString("imageUrl"),
+        )
+
+        Log.i("MyLog", "id: ${item.id}")
+        Log.i("MyLog", "title: ${item.title}")
+        Log.i("MyLog", "description: ${item.description}")
+        Log.i("MyLog", "imageUrl: ${item.imageUrl}")
+    }
+
+
 }
