9b4e3db3dec3dd9d7b7a96eb58fd3968aa9e1ca7
Maxim <jybo@mail.ru>
2024-06-25 23:58:12 +1000

    RA-36  обработка ошибки

--- app/src/main/java/com/example/androidstudyapp/data/Constants.kt
+++ app/src/main/java/com/example/androidstudyapp/data/Constants.kt
@@ -1,8 +1,6 @@
 package com.example.androidstudyapp.data
 
-const val ARG_CATEGORY_ID = "arg_category_id"
 const val FILE_COLLECTION_MY_ID = "collection_id"
 const val FAVORITE_PREFS_KEY = "favorite_prefs_key"
-const val ARG_RECIPE_ID = "arg_recipe_id"
 const val API_BASE_URL = "https://recipes.androidsprint.ru/api"
 const val API_BASE_IMAGES_URL = "$API_BASE_URL/images"
