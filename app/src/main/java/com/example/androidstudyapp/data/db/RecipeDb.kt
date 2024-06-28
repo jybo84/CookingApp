87aeb6233dcb07081b77ca08016809c7f302cc17
Maxim <jybo@mail.ru>
2024-06-28 14:54:23 +1000

    RA-39 интерфейс передан в класс

--- app/src/main/java/com/example/androidstudyapp/data/db/RecipeDb.kt
+++ app/src/main/java/com/example/androidstudyapp/data/db/RecipeDb.kt
@@ -1,6 +1,7 @@
 package com.example.androidstudyapp.data.db
 
 import android.content.Context
+import androidx.room.Dao
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
@@ -8,6 +9,8 @@ import com.example.androidstudyapp.data.Category
 
 @Database(entities = [Category::class], version = 1)
 abstract class RecipeDb : RoomDatabase() {
+
+    abstract fun getCategoriesDao(): Dao
 
     companion object {
         fun getDb(context: Context): RecipeDb {
