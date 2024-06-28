851ce4792ca81df9f109739a0460cf0cf129aed9
Maxim <jybo@mail.ru>
2024-06-28 14:19:52 +1000

    RA-39 создано Entity

--- app/src/main/java/com/example/androidstudyapp/data/Category.kt
+++ app/src/main/java/com/example/androidstudyapp/data/Category.kt
@@ -1,12 +1,20 @@
 package com.example.androidstudyapp.data
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "recipeTable")
 data class Category(
-    var id: Int,
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null,
+    @ColumnInfo(name = "title")
     val title: String,
+    @ColumnInfo(name = "description")
     val description: String,
+    @ColumnInfo(name = "imageUrl")
     val imageUrl: String,
 ) : Parcelable
