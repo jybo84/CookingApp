6b6e7eee8b6dee6890dc5de8d3ec3da49556b55b
Maxim <jybo@mail.ru>
2024-06-28 14:11:53 +1000

    RA-39 первоначальное состояние

--- app/src/main/java/com/example/androidstudyapp/data/Category.kt
+++ app/src/main/java/com/example/androidstudyapp/data/Category.kt
@@ -1,21 +1,12 @@
 package com.example.androidstudyapp.data
 
 import android.os.Parcelable
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity("category")
 data class Category(
-    @ColumnInfo("id")
-    @PrimaryKey
     var id: Int,
-    @ColumnInfo("title")
     val title: String,
-    @ColumnInfo("description")
     val description: String,
-    @ColumnInfo("image_url")
     val imageUrl: String,
 ) : Parcelable
