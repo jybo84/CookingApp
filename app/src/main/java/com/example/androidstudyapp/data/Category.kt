1915f8a4449392a84a16acbdd6e2e803c67d29c3
Maxim <jybo@mail.ru>
2024-06-29 19:56:03 +1000

    RA-39 добавлены сущности Entity  Category

--- app/src/main/java/com/example/androidstudyapp/data/Category.kt
+++ app/src/main/java/com/example/androidstudyapp/data/Category.kt
@@ -1,12 +1,20 @@
 package com.example.androidstudyapp.data
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity(tableName = "table_recipe")
 data class Category(
+    @PrimaryKey(autoGenerate = true)
     var id: Int,
+    @ColumnInfo(name = "title")
     val title: String,
+    @ColumnInfo(name = "description")
     val description: String,
+    @ColumnInfo(name = "imageUrl" )
     val imageUrl: String,
 ) : Parcelable
