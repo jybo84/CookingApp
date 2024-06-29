1183e2e41ca4ff0822b775f788a04ba42ce1d2f8
Maxim <jybo@mail.ru>
2024-06-29 20:16:46 +1000

    RA-39 создан класс базы данных

---
+++ app/src/main/java/com/example/androidstudyapp/data/db/RecipeDataBase.kt
@@ -0,0 +1,21 @@
+package com.example.androidstudyapp.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.androidstudyapp.data.Category
+
+@Database(version = 1, entities = [Category::class])
+abstract class RecipeDataBase: RoomDatabase(){
+
+    companion object{
+        fun database(context: Context): RecipeDataBase{
+            return Room.databaseBuilder(
+                context.applicationContext,
+                RecipeDataBase::class.java,
+                "applicationData"
+            ).build()
+        }
+    }
+}
