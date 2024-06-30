f098e604723c47d465103a95e33716841cc0b58f
Maxim <jybo@mail.ru>
2024-06-30 16:56:16 +1000

    RA-40 в класс БД добавлен getRecipesDao()

--- app/src/main/java/com/example/androidstudyapp/data/db/DataBase.kt
+++ app/src/main/java/com/example/androidstudyapp/data/db/DataBase.kt
@@ -5,11 +5,14 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.androidstudyapp.data.Category
+import com.example.androidstudyapp.data.Recipe
 
-@Database(version = 1, entities = [Category::class])
+@Database(version = 1, entities = [Category::class, Recipe::class])
 abstract class DataBase : RoomDatabase() {
 
     abstract fun getCategoryDao(): CategoriesDao
+
+    abstract fun getRecipesDao(): CategoriesDao
 
     companion object {
         fun getDataBase(context: Context): DataBase {
