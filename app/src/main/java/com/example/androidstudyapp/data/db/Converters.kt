aa219aa1a6c2aa9f4794e414b8b4f5e8dc8f6e0f
Maxim <jybo@mail.ru>
2024-07-01 10:12:45 +1000

    RA-40 корректировки по коду

--- app/src/main/java/com/example/androidstudyapp/data/db/Converters.kt
+++ app/src/main/java/com/example/androidstudyapp/data/db/Converters.kt
@@ -7,15 +7,15 @@ import com.google.gson.Gson
 class Converters {
 
     @TypeConverter
-    fun listToJsonString(value: List<Ingredient>?): String = Gson().toJson(value)
+    fun listToJsonStringIngredient(value: List<Ingredient>?): String = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonStringToList(value: String) = Gson().fromJson(value, Array<Ingredient>::class.java).toList()
+    fun jsonStringToListIngredient(value: String) = Gson().fromJson(value, Array<Ingredient>::class.java).toList()
 
 
     @TypeConverter
-    fun listToJsonString2(value: List<String>?): String = Gson().toJson(value)
+    fun listToJsonString(value: List<String>?): String = Gson().toJson(value)
 
     @TypeConverter
-    fun jsonStringToList2(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
+    fun jsonStringToList(value: String) = Gson().fromJson(value, Array<String>::class.java).toList()
 }
