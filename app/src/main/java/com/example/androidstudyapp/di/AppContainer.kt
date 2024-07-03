85ea611f41d8d996ec614ed43e51824e0bd596bf
Maxim <jybo@mail.ru>
2024-07-03 14:14:45 +1000

    RA-44 создан AppContainer и вынесены зависмости

---
+++ app/src/main/java/com/example/androidstudyapp/di/AppContainer.kt
@@ -0,0 +1,31 @@
+package com.example.androidstudyapp.di
+
+import com.example.androidstudyapp.data.API_BASE_URL
+import com.example.androidstudyapp.data.RecipeApiService
+import com.example.androidstudyapp.data.RecipesRepository
+import com.example.androidstudyapp.data.db.DataBase
+import com.example.androidstudyapp.ui.RecipesApplication
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class AppContainer {
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("$API_BASE_URL/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val dataBase = DataBase.getDataBase(RecipesApplication.instance)
+
+    private val recipeApiService: RecipeApiService = retrofit.create(RecipeApiService::class.java)
+
+    private val categoriesDao = dataBase.getCategoryDao()
+
+    private val recipesDao = dataBase.getRecipesDao()
+
+    val repository = RecipesRepository(
+        recipesDao = recipesDao,
+        categoriesDao = categoriesDao,
+        recipeApiService = recipeApiService
+    )
+}
