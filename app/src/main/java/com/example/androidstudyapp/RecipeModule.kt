77c0e2114658be039057320f0f3e823f34dc75b3
Maxim <jybo@mail.ru>
2024-07-15 21:07:50 +1000

    RA-45 в модуле убраны синглтоны

--- app/src/main/java/com/example/androidstudyapp/RecipeModule.kt
+++ app/src/main/java/com/example/androidstudyapp/RecipeModule.kt
@@ -14,32 +14,27 @@ import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RecipeModule() {
 
     @Provides
-    @Singleton
     fun provideRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl("$API_BASE_URL/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
     @Provides
-    @Singleton
-    fun provideDataBase( @ApplicationContext context: Context): DataBase = DataBase.getDataBase(context)
+    fun provideDataBase(@ApplicationContext context: Context): DataBase =
+        DataBase.getDataBase(context)
 
     @Provides
-    @Singleton
     fun provideRecipeApiServer(retrofit: Retrofit): RecipeApiService = retrofit.create()
 
     @Provides
-    @Singleton
     fun provideCategoriesDao(dataBase: DataBase): CategoriesDao = dataBase.getCategoryDao()
 
     @Provides
-    @Singleton
     fun provideRecipesDao(dataBase: DataBase): RecipesDao = dataBase.getRecipesDao()
 }
